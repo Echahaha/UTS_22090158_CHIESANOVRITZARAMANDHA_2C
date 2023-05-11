ef41fa268c2a6331f1234dc5ea868d98bad2e95a
Echahaha <114822509+Echahaha@users.noreply.github.com>
2023-05-11 16:37:14 +0700

    first commit

---
+++ assets/scripts/ajax/getcurl.php
@@ -0,0 +1,65 @@
+<style>
+    table {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td, th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    tr:hover {
+        background-color: #ddd;
+    }
+
+    th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #AF4C7A;
+        color: white;
+    }
+</style>
+<?php 
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL =>'https://farmasi.mimoapps.xyz/mimoqss2auyqD1EAlkgZCOhiffSsFl6QqAEIGtM',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET'
+));
+
+$response = curl_exec($curl);
+curl_close($curl);
+$response_array = json_decode($response, true);
+
+$onscreen = '<table class="table" width="100%">
+                  <thead>
+                    <th>Kode Barang</th>
+                    <th style="text-align:center">Nama Barang</th>
+                    <th style="text-align:center">Harga Jual</th>
+                    <th style="text-align:center">Quantity</th>
+                  </thead>
+                ';
+
+foreach ($response_array as $resp) {
+  if(substr($resp['i_name'], 0, 1) === "L"){
+   $onscreen .='<tr>
+                  <td>' . $resp['i_code'] . '</td>
+                  <td>' . $resp['i_name'] . '</td>
+                  <td style="text-align:center">' .'Rp. ' . $resp['i_sell'] . '</td>
+                  <td style="text-align:center">' . $resp['i_qty'] .' </td>
+                </tr>';
+}}
+$onscreen.='</table>';
+echo $onscreen;
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <style>
+        body {
+            font-family: "Quicksand", sans-serif;
+        }
+    </style>
+</head>
+
+<?php
+    require_once('./assets/requires/config.php');
+    require_once('./assets/requires/header1.php');
+?>
+
+<div class="container">
+    <div id="jdlaccl" style="font-size: 36px; color: Purple;">
+        Harber Farmasi
+    </div>
+    <div class="card shadow-sm mb-5 bg-black rounded">
+        <div class="card-footer">
+            <button class="btn btn-primary" id="btnsearch">Submit</button>
+        </div>
+    </div>
+    <div id="infoinvaccr"></div>
+</div>
+
+</html>
+<script src="./assets/scripts/js/harberweb.js"></script>
